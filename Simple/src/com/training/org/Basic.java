e5554c759f0ba904ec70b7a52f5f6ff7012dae9c
moglix <moglix@MOGNOILTU0903>
2021-07-12 10:44:15 +0530

    New Changes

--- Simple/src/com/training/org/Basic.java
+++ Simple/src/com/training/org/Basic.java
@@ -4,7 +4,8 @@ public class Basic {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("This is a simple message");
+		System.out.println("This is a simple message");
+		System.out.println("Modified ");
 	}
 
 }
